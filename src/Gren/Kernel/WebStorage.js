fb086f2a0fdfc8b3f2508e8b64db3efafcdc894b
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-07-29 09:34:49 +0200

    Support last two functions in Storage api.

--- src/Gren/Kernel/WebStorage.js
+++ src/Gren/Kernel/WebStorage.js
@@ -6,6 +6,25 @@ import Gren.Kernel.Scheduler exposing (binding, succeed)
 
 */
 
+var _WebStorage_length = function (useLocalStorage) {
+    return __Scheduler_binding(function (callback) {
+        var length = _WebStorage_getStore(useLocalStorage).length;
+
+        return callback(__Scheduler_succeed(length));
+    });
+};
+
+var _WebStorage_key = F2(function(useLocalStorage, index) {
+    return __Scheduler_binding(function (callback) {
+        var key = _WebStorage_getStore(useLocalStorage).key(index);
+
+        if (key === null) {
+            return callback(__Scheduler_succeed(__Maybe_Nothing));
+        } else {
+            return callback(__Scheduler_succeed(__Maybe_Just(key)));
+        }
+    });
+});
 
 var _WebStorage_get = F2(function(useLocalStorage, key) {
     return __Scheduler_binding(function (callback) {
